467a011ba36fb8fd2506f1bd4eeffcbaca293428
Caio Lopes <caio.w34@gmail.com>
2022-03-19 21:38:07 -0300

    Updates

---
+++ precache-manifest.dfc2521fe77d243ec18a6573db41a9cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6b397ae70c7f4d9c285be3a38be23ec",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "fff2c5624a98ca53ff79",
+    "url": "/Portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "b70c5f406b989274a73d",
+    "url": "/Portfolio/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "fff2c5624a98ca53ff79",
+    "url": "/Portfolio/static/js/2.b06cc28f.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Portfolio/static/js/2.b06cc28f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b70c5f406b989274a73d",
+    "url": "/Portfolio/static/js/main.01d241d6.chunk.js"
+  },
+  {
+    "revision": "c125469937b460e6de34",
+    "url": "/Portfolio/static/js/runtime-main.3379861c.js"
+  }
+]);
